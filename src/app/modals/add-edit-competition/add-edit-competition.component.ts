38dae6f16a20344fe42990304529c2bd8ae31f19
Imran <flexmindinnovations@outlook.com>
2024-08-10 16:58:31 +0530

    competition list, add-edit competition api integrated

--- src/app/modals/add-edit-competition/add-edit-competition.component.ts
+++ src/app/modals/add-edit-competition/add-edit-competition.component.ts
@@ -1,12 +1,89 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpErrorResponse } from '@angular/common/http';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule, FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { CalendarModule } from 'primeng/calendar';
+import { DynamicDialogRef, DynamicDialogConfig } from 'primeng/dynamicdialog';
+import { InputSwitchModule } from 'primeng/inputswitch';
+import { InputTextModule } from 'primeng/inputtext';
+import { utils } from '../../utils';
+import { ActivationType } from '../../pages/activation/activation.component';
+import { CompetitionService } from '../../services/competition/competition.service';
 
 @Component({
   selector: 'app-add-edit-competition',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, ButtonModule, InputTextModule, CalendarModule, InputSwitchModule],
   templateUrl: './add-edit-competition.component.html',
   styleUrl: './add-edit-competition.component.scss'
 })
 export class AddEditCompetitionComponent {
+  dialogData: any;
+  formGroup!: FormGroup;
+  isEditMode: boolean = false;
+  isSubmitActionLoading: boolean = false;
+  inputId = new Date().getTime() + utils.getRandomNumber();
 
+  constructor(
+    private dialogRef: DynamicDialogRef,
+    private config: DynamicDialogConfig,
+    private competitionService: CompetitionService,
+    private fb: FormBuilder,
+    private cdref: ChangeDetectorRef
+  ) { }
+
+  ngOnInit(): void {
+    this.initFormGroup();
+    this.dialogData = this.config.data;
+    this.isEditMode = this.dialogData.isEditMode;
+  }
+
+  ngAfterViewInit(): void {
+    const data = this.dialogData;
+    if (this.isEditMode && data) {
+      data['compititionDate'] = data['compititionDate'] ? new Date(data['compititionDate']) : new Date();
+      data['status'] = data['status'] === '1' ? true : false;
+      this.formGroup.patchValue(data);
+      this.cdref.detectChanges();
+    }
+  }
+
+  initFormGroup() {
+    this.formGroup = this.fb.group({
+      status: [true, ![Validators.required]],
+      compititionName: ['', [Validators.required]],
+      compititionDate: ['', [Validators.required]],
+    })
+  }
+
+  handleDialogCancel() {
+    this.dialogRef.close(false);
+  }
+
+  handleSubmitAction() {
+    this.isSubmitActionLoading = true;
+    this.formGroup.disable();
+    const formVal = this.formGroup.getRawValue();
+    formVal['compititionId'] = this.dialogData['compititionId'] ? this.dialogData['compititionId'] : 0;
+    const compititionDate = new Date(formVal['compititionDate']).toISOString();
+    formVal['status'] = formVal['status'] ? '1' : '0';
+    formVal['compititionDate'] = compititionDate;
+
+    let apiCall = this.competitionService.SaveCompetition(formVal);
+    if (this.isEditMode) apiCall = this.competitionService.updateCompetition(formVal);
+    apiCall.subscribe({
+      next: (response) => {
+        if (response) {
+          setTimeout(() => {
+            this.dialogRef.close(response);
+          })
+        }
+      },
+      error: (error: HttpErrorResponse) => {
+        this.dialogRef.close(false);
+        utils.setMessages(error.message, 'error');
+      }
+    })
+  }
 }
--- src/app/services/competition/competition.service.ts
+++ src/app/services/competition/competition.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { ApiCallConfig, AppConfigService } from '../config/app-config.service';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CompetitionService {
 
-  constructor() { }
+  competitionApiPath: ApiCallConfig['competition'];
+
+  constructor(
+    private apiConfig: AppConfigService,
+    private http: HttpClient
+  ) {
+    this.competitionApiPath = apiConfig.get('competition');
+  }
+
+  getCompetitionList(): Observable<any> {
+    return this.http.get(this.competitionApiPath.competitionList);
+  }
+  getCompetitionById(competitionId: string): Observable<any> {
+    const getByIdApiPath = this.competitionApiPath.competitionById.replace(/\s+/g, '').replace(/\u200B/g, '').replace('{{id}}', competitionId);
+
+    return this.http.get(getByIdApiPath);
+  }
+
+  SaveCompetition(payload: any): Observable<any> {
+    return this.http.post(this.competitionApiPath.saveCompetition, payload);
+  }
+
+  updateCompetition(payload: any): Observable<any> {
+    return this.http.put(this.competitionApiPath.updateCompetition, payload);
+  }
 }
